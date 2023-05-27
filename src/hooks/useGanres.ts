180ce83fd6ebb4383297c7f0cf97bdaf622f0b78
DorielLihod <duriel35@gmail.com>
2023-05-27 11:55:26 +0300

    create a generic fetching hook

--- src/hooks/useGanres.ts
+++ src/hooks/useGanres.ts
@@ -1,41 +1,11 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Ganre {
     id: number;
     name: string;
 }
 
-interface GanresResponse {
-  count: number;
-  results: Ganre[];
-}
 
-const useGanres = () =>{
-  const [ganres, setGanres] = useState<Ganre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<GanresResponse>("/genres",{signal: controller.signal})
-      .then((res) => {
-        setGanres(res.data.results);
-        setLoading(false);
-    })
-      .catch((err) => {
-        if(err instanceof CanceledError ) return;
-        setError(err.message)
-        setLoading(false);
-    });
-      return () => controller.abort();
-  }, []);
-  return {ganres ,error , isLoading};
-}
+const useGanres = () => useData<Ganre>("/genres");
 
 export default useGanres;
