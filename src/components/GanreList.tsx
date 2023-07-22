dd4e212217fc6c191b3818d0a4b9029d057a0406
DorielLihod <duriel35@gmail.com>
2023-07-22 17:25:23 +0300

    Fix the ganres menu

--- src/components/GanreList.tsx
+++ src/components/GanreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -22,27 +23,35 @@ const GanreList = ({ onSelectGanre, selectedGanre }: Props) => {
   if (error) return null;
 
   return (
-    <List>
-      {data.map((ganre) => (
-        <ListItem key={ganre.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(ganre.image_background)}
-            />
-            <Button
-              onClick={() => onSelectGanre(ganre)}
-              fontSize="lg"
-              variant="link"
-              fontWeight={ganre.id === selectedGanre?.id ? "bold" : "normal"}
-            >
-              {ganre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Ganres
+      </Heading>
+      <List>
+        {data.map((ganre) => (
+          <ListItem key={ganre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                objectFit={"cover"}
+                src={getCroppedImageUrl(ganre.image_background)}
+              />
+              <Button
+                onClick={() => onSelectGanre(ganre)}
+                fontSize="lg"
+                variant="link"
+                fontWeight={ganre.id === selectedGanre?.id ? "bold" : "normal"}
+                whiteSpace={"normal"}
+                textAlign={"left"}
+              >
+                {ganre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
